61f08a66854127205e93d4c2e604831b6c3a5b69
prasanth reddy thumma <prasanthreddy770@gmail.com>
2020-06-10 19:08:36 -0400

    Info about city

--- app/src/main/java/com/example/weatherapp/CityInfo.java
+++ app/src/main/java/com/example/weatherapp/CityInfo.java
@@ -1,4 +1,16 @@
 package com.example.weatherapp;
 
+import java.util.List;
+
 public class CityInfo {
+    private List<WeatherInfo> consolidated_weather;
+    private String title;
+    private int woeid;
+
+    public CityInfo(List<WeatherInfo> consolidated_weather, String title, int woeid) {
+        this.consolidated_weather = consolidated_weather;
+        this.title = title;
+        this.woeid = woeid;
+    }
+
 }
