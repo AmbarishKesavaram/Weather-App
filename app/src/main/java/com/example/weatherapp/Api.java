d7b02cdbac08661bca1b678a3d2bfe7f082c59e9
sanjeev <sanjeevyerramsetty@gmail.com>
2020-06-12 11:50:08 -0400

    Api Coded

--- app/src/main/java/com/example/weatherapp/Api.java
+++ app/src/main/java/com/example/weatherapp/Api.java
@@ -1,4 +1,24 @@
 package com.example.weatherapp;
 
+
+import retrofit2.Call;
+import retrofit2.http.GET;
 public interface Api {
+
+    String BASE_URL ="https://www.metaweather.com/api/location/";
+
+    @GET("3534")
+    Call<CityInfo> getMontrealDetails();
+
+    @GET("2459115")
+    Call<CityInfo> getNewYorkDetails();
+
+    @GET("44418")
+    Call<CityInfo> getLondonDetails();
+
+    @GET("615702")
+    Call<CityInfo> getParisDetails();
+
+
+
 }
