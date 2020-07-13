4848f832b26b1e5d0a3f5b7293b345b0367fb6e0
raghu <raghu.naluvala@yahoo.com>
2020-07-13 18:03:39 -0400

    updated LondonFragment.java

--- app/src/main/java/com/example/weatherapp/LondonFragment.java
+++ app/src/main/java/com/example/weatherapp/LondonFragment.java
@@ -3,62 +3,109 @@ package com.example.weatherapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LondonFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class LondonFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public LondonFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LondonFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LondonFragment newInstance(String param1, String param2) {
-        LondonFragment fragment = new LondonFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    TextView londonHeaderText, londonWeatherStateName, londonDayOfWeek, londonMaximumTemperature, londonActualTemperature, londonMinimumTemperature, londonHumidity, londonPredictability;
+    ImageView todayWeatherIcon;
+    private RecyclerView recyclerView;
+    RecyclerAdaptor recyclerAdaptor;
+    CityInfo london;
+    FragmentTransaction fragmentTransaction;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_london, container, false);
+        View view = inflater.inflate(R.layout.fragment_london, container, false);
+        retroFitLondon();
+        return view;
     }
+
+    public void retroFitLondon() {
+
+        Api api = RetrofitInstance.getRetrofit().create(Api.class);
+        final Call<CityInfo> callLondon = api.getLondonDetails();
+        callLondon.enqueue(new Callback<CityInfo>() {
+            @Override
+            public void onResponse(Call<CityInfo> call, Response<CityInfo> response) {
+                london = response.body();
+
+                setTextView(londonHeaderText, R.id.londonHead, london.getTitle());
+                setImageView(todayWeatherIcon, R.id.londonWeatherIcon, london.getConsolidated_weather().get(0).getWeather_state_abbr());
+                setTextView(londonDayOfWeek, R.id.londonDayOfWeek, WeatherSupport.dayOfWeek(london.getConsolidated_weather().get(0).getApplicable_date()));
+                setTextView(londonWeatherStateName, R.id.londonWeatherState, london.getConsolidated_weather().get(0).getWeather_state_name());
+                setTextView(londonMaximumTemperature, R.id.londonMaxTemp, WeatherSupport.convertDouble(london.getConsolidated_weather().get(0).getMax_temp()));
+                setTextView(londonActualTemperature, R.id.londonActualTemp, WeatherSupport.convertDouble(london.getConsolidated_weather().get(0).getThe_temp()));
+                setTextView(londonMinimumTemperature, R.id.londonMinTemp, WeatherSupport.convertDouble(london.getConsolidated_weather().get(0).getMin_temp()));
+                setTextView(londonHumidity, R.id.londonHumidity, WeatherSupport.convertInt(london.getConsolidated_weather().get(0).getHumidity()));
+                setTextView(londonPredictability, R.id.londonPredictability, WeatherSupport.convertInt(london.getConsolidated_weather().get(0).getPredictability()));
+
+                generateView(london.getConsolidated_weather(), getView());
+            }
+
+            @Override
+            public void onFailure(Call<CityInfo> call, Throwable t) {
+                Toast.makeText(getActivity().getApplicationContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void setTextView(TextView textView, int id, String text) {
+
+        textView = getView().findViewById(id);
+        textView.setText(text);
+    }
+
+    private void setImageView(ImageView imageView, int id, String weatherState) {
+
+        imageView = getView().findViewById(id);
+        Picasso.get().load("https://www.metaweather.com/static/img/weather/ico/" + weatherState + ".ico").into(imageView);
+
+    }
+
+    public void generateView(ArrayList<WeatherInfo> weatherInfos, View view) {
+        weatherInfos.remove(0);
+        recyclerAdaptor = new RecyclerAdaptor(weatherInfos, getActivity().getApplicationContext());
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity().getApplicationContext(), LinearLayoutManager.VERTICAL, false);
+        recyclerView = view.findViewById(R.id.londonRecyclerView);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(recyclerAdaptor);
+        recyclerAdaptor.setOnClickListener(onClickListener);
+    }
+
+    public View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            RecyclerView.ViewHolder viewHolder = (RecyclerView.ViewHolder) v.getTag();
+            int position = viewHolder.getAdapterPosition();
+            Bundle b = new Bundle();
+            b.putParcelable("weather", london.getConsolidated_weather().get(position));
+            b.putString("city","London");
+            PredictionFragment fragment = new PredictionFragment();
+            fragment.setArguments(b);
+
+            fragmentTransaction = getFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
+        }
+    };
 }
