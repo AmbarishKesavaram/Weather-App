74b28eaa8845a6b109053b75675e0d865643a7ee
raghu <raghu.naluvala@yahoo.com>
2020-07-13 21:23:48 -0400

    updated pridictionfragment.java

--- app/src/main/java/com/example/weatherapp/PredictionFragment.java
+++ app/src/main/java/com/example/weatherapp/PredictionFragment.java
@@ -2,63 +2,73 @@ package com.example.weatherapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PredictionFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.squareup.picasso.Picasso;
+
+
 public class PredictionFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    TextView  weatherStateName,header, maximumTemperature,dayOfWeek, actualTemperature, minimumTemperature, humidity, predictability;
+    ImageView weatherIcon;
+    WeatherInfo weatherInfo;
 
     public PredictionFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PredictionFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PredictionFragment newInstance(String param1, String param2) {
-        PredictionFragment fragment = new PredictionFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_prediction, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        weatherInfo =getArguments().getParcelable("weather");
+
+
+        generateView();
+    }
+
+    public void generateView() {
+        String head =getArguments().getString("city");
+        setTextView(weatherStateName,R.id.weatherState,weatherInfo.getWeather_state_name());
+        setImageView(weatherIcon,R.id.weatherIcon,weatherInfo.getWeather_state_abbr());
+        setTextView(maximumTemperature,R.id.maxTemp,WeatherSupport.convertDouble(weatherInfo.getMax_temp()));
+        setTextView(actualTemperature,R.id.actualTemp,WeatherSupport.convertDouble(weatherInfo.getThe_temp()));
+        setTextView(minimumTemperature,R.id.minTemp,WeatherSupport.convertDouble(weatherInfo.getMin_temp()));
+        setTextView(humidity,R.id.humidity,WeatherSupport.convertInt(weatherInfo.getHumidity()));
+        setTextView(predictability,R.id.predictability,WeatherSupport.convertInt(weatherInfo.getPredictability()));
+        setTextView(dayOfWeek,R.id.prDayOfWeek,WeatherSupport.dayOfWeek(weatherInfo.getApplicable_date()));
+        setTextView(header,R.id.head,head);
+    }
+
+    private void setTextView(TextView textView, int id, String text) {
+
+        textView = getView().findViewById(id);
+        textView.setText(text);
+    }
+
+    private void setImageView(ImageView imageView, int id, String weatherState) {
+
+        imageView = getView().findViewById(id);
+        Picasso.get().load("https://www.metaweather.com/static/img/weather/ico/" + weatherState + ".ico").into(imageView);
+
+    }
+
 }
